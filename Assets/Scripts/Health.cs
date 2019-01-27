54bba6241f6f4bc0eee4249bfa97798258901c7e
Iman Toumi <reyan.toumi@gmail.com>
2019-01-27 17:35:52 +0400

    Multi bounce on wall

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField] int maxHealth = 100;
+
+    private int health;
+
+    private void Start() {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int amount) {
+        health -= amount;
+
+        if (health <= 0) {
+            // Die
+        }
+    }
+}
--- Assets/Scripts/PlayerDash.cs
+++ Assets/Scripts/PlayerDash.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class PlayerDash : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     [SerializeField] float dashSpeed = 1f;
     [SerializeField] float dashDistance = 2f;
@@ -19,22 +19,12 @@ public class PlayerDash : MonoBehaviour {
         mainCamera = Camera.main;
         velocity = dashDistance / dashSpeed;
         colliderRadius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+        destination = rb2D.position;
     }
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 dashDirection = (mousePosition - rb2D.position).normalized;
-            RaycastHit2D hit = Physics2D.CircleCast(rb2D.position - (dashDirection * circleCastOffset),
-                colliderRadius, dashDirection, dashDistance);
-            if (hit) {
-                destination = hit.centroid;
-                remainingMovement = rb2D.position - hit.centroid + (dashDirection * dashDistance);
-                remainingMovement = Vector2.Reflect(remainingMovement, hit.normal);
-                doPositionCheck = true;
-            } else {
-                destination = rb2D.position + (dashDirection * dashDistance);
-            }
+            Dash();
         }
         rb2D.MovePosition(Vector2.MoveTowards(rb2D.position, destination, velocity * Time.deltaTime));
     }
@@ -45,8 +35,31 @@ public class PlayerDash : MonoBehaviour {
                 return;
             }
 
-            destination += remainingMovement;
-            doPositionCheck = false;
+            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.CircleCast(rb2D.position, colliderRadius, remainingMovement, remainingMovement.magnitude);
+            if (hit) {
+                destination = hit.centroid;
+                remainingMovement = rb2D.position - hit.centroid + remainingMovement;
+                remainingMovement = Vector2.Reflect(remainingMovement, hit.normal);
+            } else {
+                destination += remainingMovement;
+                doPositionCheck = false;
+            }
+        }
+    }
+
+    private void Dash() {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dashDirection = (mousePosition - rb2D.position).normalized;
+        RaycastHit2D hit = Physics2D.CircleCast(rb2D.position - (dashDirection * circleCastOffset),
+            colliderRadius, dashDirection, dashDistance + circleCastOffset);
+        if (hit) {
+            destination = hit.centroid;
+            remainingMovement = rb2D.position - hit.centroid + (dashDirection * dashDistance);
+            remainingMovement = Vector2.Reflect(remainingMovement, hit.normal);
+            doPositionCheck = true;
+        } else {
+            destination = rb2D.position + (dashDirection * dashDistance);
         }
     }
 }
