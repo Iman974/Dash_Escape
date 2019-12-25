9f12c21167aad11bde0fe08ddeca81a9e9c0fc59
Iman Toumi <reyan.toumi@gmail.com>
2019-12-25 18:00:45 +0400

    Moved dash back to playerController2D. Fixed dash when to close to wall

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -5,18 +5,15 @@ public class Controller2D : MonoBehaviour {
 
     [SerializeField] RaycastController raycastController = null;
 
-    public CollisionsInfo collisions;
+    public Collisions collisions;
 
     RaycastController.RaycastOrigins raycastOrigins;
     LayerMask collisionLayer;
-
-    const int Down = -1, Left = -1;
 
     void Start() {
         raycastController.Initialize(GetComponent<Collider2D>());
         raycastOrigins = raycastController.Origins;
         collisionLayer = raycastController.CollisionLayer;
-        dashVelocity = dashDistance / dashSpeed;
     }
 
     public void Move(Vector2 deltaMove) {
@@ -24,50 +21,25 @@ public class Controller2D : MonoBehaviour {
         collisions.Reset();
 
         if (!Mathf.Approximately(deltaMove.x, 0f)) {
-            ProcessHorizontalCollisions(ref deltaMove);
+            CastHorizontalRays(deltaMove);
+            if (collisions.hitX) {
+                deltaMove.x = collisions.hitX.distance * (collisions.right ? 1f : -1f);
+            }
         }
         if (!Mathf.Approximately(deltaMove.y, 0f)) {
-            ProcessVerticalCollisions(ref deltaMove);
+            CastVerticalRays(deltaMove);
+            if (collisions.hitY) {
+                deltaMove.y = collisions.hitY.distance * (collisions.above ? 1f : -1f);
+            }
         }
         transform.position += (Vector3)deltaMove;
     }
 
-    [SerializeField] float dashDistance = 4f;
-    [SerializeField] float dashSpeed = 0.14f;
-
-    Vector2 dashDirection;
-    Vector2 dashStartPosition;
-    float remainingDashDistance;
-    float dashVelocity;
-
-    public void InitDash() {
-        dashStartPosition = transform.position;
-        remainingDashDistance = dashDistance;
-        dashDirection = (InputUtility.GetWorldMousePosition() - dashStartPosition).normalized;
-    }
-
-    public bool Dash() {
-        if ((dashStartPosition - (Vector2)transform.position).sqrMagnitude >= remainingDashDistance *
-        remainingDashDistance) {
-            return false;
-        }
-        if (collisions.right || collisions.left || collisions.above || collisions.below) {
-            Wall wall = collisions.hitCollider.GetComponent<Wall>();
-            Vector2 normal = collisions.normal;
-            if (wall.DashCollision == Wall.DashReaction.Bounce) {
-                dashDirection = Vector2.Reflect(dashDirection, normal);
-                remainingDashDistance -= (dashStartPosition - (Vector2)transform.position).magnitude;
-                dashStartPosition = transform.position;
-            } else if (wall.DashCollision == Wall.DashReaction.Stop) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    void ProcessHorizontalCollisions(ref Vector2 deltaMove) {
+    // This is the exact same function as the one below -> use delegate to make only one function ?
+    void CastHorizontalRays(Vector2 deltaMove) {
         float directionX = Mathf.Sign(deltaMove.x);
         float rayLength = Mathf.Abs(deltaMove.x) + RaycastController.SkinWidth;
+        const int Left = -1;
 
         for (int i = 0; i < raycastController.HorizontalRayCount; i++) {
             Vector2 rayOrigin = (int)directionX == Left ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
@@ -77,50 +49,62 @@ public class Controller2D : MonoBehaviour {
             Debug.DrawRay(rayOrigin, Vector2.right * directionX, Color.red);
 
             if (hit) {
-                deltaMove.x = (hit.distance - RaycastController.SkinWidth) * directionX;
                 rayLength = hit.distance;
-
+                hit.distance -= RaycastController.SkinWidth;
+                collisions.hitX = hit;
                 collisions.left = (int)directionX == Left;
                 collisions.right = !collisions.left;
-                collisions.hitCollider = hit.collider;
-                collisions.normal = hit.normal;
             }
         }
     }
 
-    void ProcessVerticalCollisions(ref Vector2 deltaMove) {
+    // This is the exact same function as the one above -> use delegate to make only one function ?
+    void CastVerticalRays(Vector2 deltaMove) {
         float directionY = Mathf.Sign(deltaMove.y);
         float rayLength = Mathf.Abs(deltaMove.y) + RaycastController.SkinWidth;
+        const int Down = -1;
 
         for (int i = 0; i < raycastController.VerticalRayCount; i++) {
             Vector2 rayOrigin = (int)directionY == Down ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
-            rayOrigin += Vector2.right * ((raycastController.VerticalRaySpacing * i) + deltaMove.x);
+            rayOrigin += Vector2.right * ((raycastController.VerticalRaySpacing * i) /*+ deltaMove.x*/);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionLayer);
 
             Debug.DrawRay(rayOrigin, Vector2.up * directionY, Color.blue);
 
             if (hit) {
-                deltaMove.y = (hit.distance - RaycastController.SkinWidth) * directionY;
                 rayLength = hit.distance;
-
+                hit.distance -= RaycastController.SkinWidth;
+                collisions.hitY = hit;
                 collisions.below = (int)directionY == Down;
                 collisions.above = !collisions.below;
-                collisions.hitCollider = hit.collider;
-                collisions.normal = hit.normal;
             }
         }
     }
 
-    public struct CollisionsInfo {
+    public void CastAllRays(Vector2 deltaMove) {
+        CastHorizontalRays(deltaMove);
+        CastVerticalRays(deltaMove);
+    }
+
+    public struct Collisions {
         public bool above, below;
         public bool left, right;
-        public Collider2D hitCollider;
-        public Vector2 normal;
+        public RaycastHit2D hitX;
+        public RaycastHit2D hitY;
 
         public void Reset() {
             above = below = false;
             left = right = false;
-            hitCollider = null;
+            hitX = default;
+            hitY = default;
+        }
+
+        // Returns the hit which touched a collider. If none returns hitY.
+        public RaycastHit2D GetAnyHit() {
+            if (hitX) {
+                return hitX;
+            }
+            return hitY;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,27 @@
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 6f;
-    [SerializeField] RaycastController raycastController;
+    [SerializeField] float dashDistance = 4f;
+    [SerializeField] float dashDuration = 0.14f;
+    [SerializeField] float minDashDistance = 0.05f;
 
-    Vector2 input;
     Rigidbody2D rb2D;
     Controller2D controller2D;
-    bool isDashing;
     Camera mainCamera;
+
+    Vector2 input;
+    bool isDashing;
+    Vector2 dashDirection;
+    Vector2 dashStartPosition;
+    float remainingDashDistance;
+    float dashVelocity;
 
     void Start() {
         rb2D = GetComponent<Rigidbody2D>();
         controller2D = GetComponent<Controller2D>();
         mainCamera = Camera.main;
+
+        dashVelocity = dashDistance / dashDuration;
     }
 
     void Update() {
@@ -22,14 +31,42 @@ public class PlayerController : MonoBehaviour {
         input.y = Input.GetAxisRaw("Vertical");
 
         if (Input.GetMouseButtonDown(0)) {
-            isDashing = true;
-            controller2D.InitDash();
+            InitDash();
         }
 
         if (!isDashing) {
             controller2D.Move(input * moveSpeed * Time.deltaTime);
         } else {
-            isDashing = controller2D.Dash();
+            controller2D.Move(dashDirection * dashVelocity * Time.deltaTime);
+            HandleDash();
+        }
+    }
+
+    void InitDash() {
+        dashStartPosition = transform.position;
+        remainingDashDistance = dashDistance;
+        dashDirection = (InputUtility.GetWorldMousePosition() - dashStartPosition).normalized;
+        controller2D.CastAllRays(dashDirection * minDashDistance);
+        var collisions = controller2D.collisions;
+        isDashing = !(collisions.hitX || collisions.hitY);
+    }
+
+    void HandleDash() {
+        if ((dashStartPosition - (Vector2)transform.position).sqrMagnitude >=
+                remainingDashDistance * remainingDashDistance) {
+            isDashing = false;
+        }
+
+        RaycastHit2D hit = controller2D.collisions.GetAnyHit();
+        if (hit) {
+            Wall wall = hit.collider.GetComponent<Wall>();
+            if (wall.DashCollision == Wall.DashReaction.Bounce) {
+                dashDirection = Vector2.Reflect(dashDirection, hit.normal);
+                remainingDashDistance -= (dashStartPosition - (Vector2)transform.position).magnitude;
+                dashStartPosition = transform.position;
+            } else if (wall.DashCollision == Wall.DashReaction.Stop) {
+                isDashing = false;
+            }
         }
     }
 }
